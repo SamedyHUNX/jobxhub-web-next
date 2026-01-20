684b448cebb7245e7a561f0a77b57cf883febf47
samedyhunx <samedy.hunx@gmail.com>
2026-01-20 18:40:50 +0700

    initial react-redux and reduxjs setup

---
+++ store/slices/auth.slice.ts
@@ -0,0 +1,41 @@
+import { User } from "@/types";
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { RootState } from "../stores";
+
+interface AuthState {
+  user: User | null;
+  token: string | null;
+  isInitialized: boolean;
+}
+
+const initialState: AuthState = {
+  user: null,
+  token: null,
+  isInitialized: false,
+};
+
+const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {
+    setAuth: (state, action: PayloadAction<{ token: string; user: User }>) => {
+      state.token = action.payload.token;
+      state.user = action.payload.user;
+      state.isInitialized = true;
+    },
+    clearAuth: (state) => {
+      state.user = null;
+      state.token = null;
+      state.isInitialized = true;
+    },
+    markInitialized: (state) => {
+      state.isInitialized = true;
+    },
+  },
+});
+
+export const { setAuth, clearAuth, markInitialized } = authSlice.actions;
+
+export const selectIsAuthenticated = (state: RootState) => !!state.auth.token;
+
+export default authSlice.reducer;
--- types/index.ts
+++ types/index.ts
@@ -1,1 +1,15 @@
+// Locale
 export type LocaleType = "en" | "de" | "km";
+
+// User
+export interface User {
+  id: string;
+  email: string;
+  username: string;
+  firstName: string;
+  lastName: string;
+  imageUrl: string;
+  userRole: string;
+  token: string;
+  dateOfBirth: string;
+}
