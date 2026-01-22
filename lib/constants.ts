3319b366e95a926261411c7c6ef9eda982c73f81
samedyhunx <samedy.hunx@gmail.com>
2026-01-22 22:47:07 +0700

    signup page

---
+++ lib/constants.ts
@@ -0,0 +1,13 @@
+// Country codes data
+export const countries = [
+  { code: "+61", name: "Australia", flag: "🇦🇺" },
+  { code: "+855", name: "Cambodia", flag: "🇰🇭" },
+  { code: "+1", name: "USA", flag: "🇺🇸" },
+  { code: "+44", name: "UK", flag: "🇬🇧" },
+  { code: "+66", name: "Thailand", flag: "🇹🇭" },
+  { code: "+84", name: "Vietnam", flag: "🇻🇳" },
+  { code: "+86", name: "China", flag: "🇨🇳" },
+  { code: "+81", name: "Japan", flag: "🇯🇵" },
+  { code: "+82", name: "South Korea", flag: "🇰🇷" },
+  { code: "+65", name: "Singapore", flag: "🇸🇬" },
+];
