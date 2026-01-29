11b6253a118d1da246ac116e6f2397bf67c9e5ee
samedyhunx <samedy.hunx@gmail.com>
2026-01-29 22:29:14 +0700

    getOrgId page

--- app/[locale]/employer/orgs/[orgId]/page.tsx
+++ app/[locale]/employer/orgs/[orgId]/page.tsx
@@ -1,3 +1,8 @@
+"use client";
+
+import { useParams } from "next/navigation";
+
 export default function OrgByIdPage() {
-  return null;
+  const { orgId } = useParams();
+  return <h1 className="text-white">{orgId}</h1>;
 }
