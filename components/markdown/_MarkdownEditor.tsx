7c8f86337fc65621c927b35fc0c0c10c75fcf2d8
samedyhunx <samedy.hunx@gmail.com>
2026-01-29 22:37:13 +0700

    markdown component

---
+++ components/markdown/_MarkdownEditor.tsx
@@ -0,0 +1,62 @@
+"use client";
+
+import { cn } from "@/lib/utils";
+import {
+  MDXEditorProps,
+  MDXEditorMethods,
+  MDXEditor,
+  headingsPlugin,
+  listsPlugin,
+  quotePlugin,
+  thematicBreakPlugin,
+  markdownShortcutPlugin,
+  tablePlugin,
+  toolbarPlugin,
+  BlockTypeSelect,
+  BoldItalicUnderlineToggles,
+  ListsToggle,
+  InsertThematicBreak,
+  InsertTable,
+} from "@mdxeditor/editor";
+import { useTheme } from "next-themes";
+import { Ref } from "react";
+
+export const markdownClassNames =
+  "max-w-none prose prose-neutral dark:prose-invert font-sans";
+
+export default function InternalMarkdownEditor({
+  ref,
+  className,
+  ...props
+}: MDXEditorProps & { ref?: Ref<MDXEditorMethods> }) {
+  const { theme, resolvedTheme } = useTheme();
+  const isDark = theme === "dark" || resolvedTheme === "dark";
+
+  return (
+    <MDXEditor
+      {...props}
+      ref={ref}
+      className={cn(markdownClassNames, isDark && "dark-theme", className)}
+      suppressHtmlProcessing
+      plugins={[
+        headingsPlugin(),
+        listsPlugin(),
+        quotePlugin(),
+        thematicBreakPlugin(),
+        markdownShortcutPlugin(),
+        tablePlugin(),
+        toolbarPlugin({
+          toolbarContents: () => (
+            <>
+              <BlockTypeSelect />
+              <BoldItalicUnderlineToggles />
+              <ListsToggle />
+              <InsertThematicBreak />
+              <InsertTable />
+            </>
+          ),
+        }),
+      ]}
+    />
+  );
+}
