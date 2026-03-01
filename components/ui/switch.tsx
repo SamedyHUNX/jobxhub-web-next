272bf240d456b4f485177fbbf924548e5a1aec75
samedyhunx <samedy.hunx@gmail.com>
2026-03-01 16:48:36 +0700

    fixed thumb color

--- components/ui/switch.tsx
+++ components/ui/switch.tsx
@@ -1,4 +1,3 @@
-// components/ui/switch.tsx
 "use client";
 
 import * as React from "react";
@@ -20,7 +19,9 @@ const Switch = React.forwardRef<
   >
     <SwitchPrimitives.Thumb
       className={cn(
-        "pointer-events-none block h-5 w-5 rounded-full bg-background shadow-lg ring-0 transition-transform data-[state=checked]:translate-x-5 data-[state=unchecked]:translate-x-0",
+        "pointer-events-none block h-5 w-5 rounded-full shadow-lg ring-0 transition-transform",
+        "bg-white",
+        "data-[state=checked]:translate-x-5 data-[state=unchecked]:translate-x-0",
       )}
     />
   </SwitchPrimitives.Root>
